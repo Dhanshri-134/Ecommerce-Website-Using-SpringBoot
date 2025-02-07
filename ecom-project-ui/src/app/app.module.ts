fb85430cb93e5644d9f2da4566a9a0c6d4f4af3d
Dhanshri-134 <yewankardhanshri@gmail.com>
2025-02-07 09:33:11 +0530

    Added Some Functionalities of login form done by Samiksha

--- ecom-project-ui/src/app/app.module.ts
+++ ecom-project-ui/src/app/app.module.ts
@@ -1,25 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { AdminComponent } from './admin/admin.component';
-import { UserComponent } from './user/user.component';
-import { LoginComponent } from './login/login.component';
-import { HeaderComponent } from './header/header.component';
-import { ForbiddenComponent } from './forbidden/forbidden.component';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
+
 import { AuthGuard } from './_auth/auth.guard';
 import { AuthInterceptor } from './_auth/auth.interceptor';
 import { UserService } from './_services/user.service';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-// Dhanshri's Edition
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
-//End of Dhanshri's Edition
+
+import { AdminComponent } from './admin/admin.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ForbiddenComponent } from './forbidden/forbidden.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
@@ -38,17 +40,18 @@ import {MatButtonModule} from '@angular/material/button';
     HttpClientModule,
     RouterModule,
     BrowserAnimationsModule,
-    // Dhanshri's Edition
     MatToolbarModule,
     MatButtonModule,
-    // End of Dhanshri's Edition
+    MatFormFieldModule,  // Moved here
+    MatInputModule,      // Moved here
+    MatIconModule        // Moved here
   ],
   providers: [
     AuthGuard,
     {
       provide: HTTP_INTERCEPTORS,
-      useClass:AuthInterceptor,
-      multi:true
+      useClass: AuthInterceptor,
+      multi: true
     },
     UserService
   ],
