45570fb0383bcad08ad2bc9640ed36393103ccb0
SamikshaHarel <harelsamiksha127@gmail.com>
2025-04-07 19:38:43 +0530

    Fixed merge conflicts in product.service.ts

--- ecom-project-ui/src/app/_services/product.service.ts
+++ ecom-project-ui/src/app/_services/product.service.ts
@@ -1,44 +1,36 @@
-
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Product } from '../_model/product.model';  // Adjust path if necessary
 import { OrderDetails } from '../_model/order-details.model';
-
+import { Product } from '../_model/product.model'; // Adjust path if necessary
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor( private httpClient:HttpClient) { }
-  public addProduct(product:FormData){
-    return this.httpClient.post<Product>("http://localhost:9090/addNewProduct/",product);
-  }
-  public getAllProducts(pageNumber){
-    return this.httpClient.get<Product[]>("http://localhost:9090/getAllProducts?pageNumber="+pageNumber);
-  }
-  public getProductDetailsById(productId){
-    return this.httpClient.get<Product>("http://localhost:9090/getProductDetailsById/"+productId);
-  }
-  public deleteProduct(productId: number){
-    return this.httpClient.delete("http://localhost:9090/deleteProductDetails/"+productId);
-<<<<<<< HEAD
-}
-public getProductDetails(isSingleProductCheckout,productId){
-  return this.httpClient.get<Product[]>("http://localhost:9090/getProductDetails/"+isSingleProductCheckout+"/"+productId);
+  constructor(private httpClient: HttpClient) { }
 
-}
-public placeOrder(orderDetails: OrderDetails){
-  return this.httpClient.post("http://localhost:9090//placeOrder",orderDetails);
-}
-=======
+  public addProduct(product: FormData) {
+    return this.httpClient.post<Product>("http://localhost:9090/addNewProduct/", product);
   }
-  public getProductDetails(isSingleProductCheckout,productId){
-    return this.httpClient.get<Product[]>("http://localhost:9090/getProductDetails/"+isSingleProductCheckout+"/"+productId);
-  } 
-  public placeOrder(orderDetails: OrderDetails){
-    return this.httpClient.post("http://localhost:9090//placeOrder",orderDetails);
-  }
->>>>>>> 0fc15ad2b77e54d12989640f3810ac410e689ec3
-}
 
+  public getAllProducts(pageNumber) {
+    return this.httpClient.get<Product[]>("http://localhost:9090/getAllProducts?pageNumber=" + pageNumber);
+  }
+
+  public getProductDetailsById(productId) {
+    return this.httpClient.get<Product>("http://localhost:9090/getProductDetailsById/" + productId);
+  }
+
+  public deleteProduct(productId: number) {
+    return this.httpClient.delete("http://localhost:9090/deleteProductDetails/" + productId);
+  }
+
+  public getProductDetails(isSingleProductCheckout, productId) {
+    return this.httpClient.get<Product[]>("http://localhost:9090/getProductDetails/" + isSingleProductCheckout + "/" + productId);
+  }
+
+  public placeOrder(orderDetails: OrderDetails) {
+    return this.httpClient.post("http://localhost:9090/placeOrder", orderDetails);
+  }
+}
